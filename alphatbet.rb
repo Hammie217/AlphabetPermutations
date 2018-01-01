7d5828ac73918c9a398e8faff14068aa26951289
Hammie217 <hcsams1@sheffield.ac.uk>
2018-01-01 13:01:11 +0000

    initial

---
+++ alphatbet.rb
@@ -0,0 +1,5 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+#needs to output every possible order of writing the alphabet(no repeating characters)
+arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].permutation.to_a
+
+print arr
